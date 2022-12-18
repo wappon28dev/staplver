4be149c22ba8d92e9ecc510c3dd9f5b68d7fe1a6
wappon_28_dev <wappon_28_dev@ruru.be>
2022-12-18 21:37:06 +0900

    💄 フォントをローカルでバンドル

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import dynamic_color
-import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
